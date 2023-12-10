18cb200b6ac6e32b64513c0c7d9f933dcc8a2f09
0xf00ff00f <fzorto@gmail.com>
2023-12-09 23:17:08 -0300

    Test: inline panic

--- tests/panic.h
+++ tests/panic.h
@@ -1,14 +1,14 @@
 #include <cstdio>
 #include <cstdlib>
 
-void panic(const char *fmt)
+inline void panic(const char *fmt)
 {
     std::fprintf(stderr, "%s", fmt);
     std::abort();
 }
 
 template<typename... Args>
-void panic(const char *fmt, const Args &...args)
+inline void panic(const char *fmt, const Args &...args)
 {
     std::fprintf(stderr, fmt, args...);
     std::abort();
